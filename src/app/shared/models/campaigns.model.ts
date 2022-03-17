8a21a3f5a51408510c64e671831598dc7c1b9fa1
RPGToreta <vitriolscret@naver.com>
2022-03-17 12:44:52 +0900

    message

---
+++ src/app/shared/models/campaigns.model.ts
@@ -0,0 +1,81 @@
+export interface Campaign {
+  title: string;
+  label: string;
+  platform: string;
+  npcs: NPC[];
+  logs: Log[];
+}
+
+export interface NPC {
+  name: string;
+  avatar: string;
+}
+
+export interface Log {
+  index: string;
+  name: string;
+  images?: Image[];
+  interfaces?: Image[];
+}
+
+export interface Image {
+  id: string;
+  file: string;
+}
+
+export const Campaigns = [
+  {
+    title: '',
+    label: '',
+    platform: 'FVTT',
+    npcs: [
+      {
+        name: '',
+        avatar: '',
+      },
+    ],
+    logs: [
+      {
+        index: '',
+        name: '',
+        images: [
+          {
+            id: ``,
+            file: '',
+          },
+        ],
+        interfaces: [
+          {
+            id: ``,
+            file: '',
+          },
+        ],
+      },
+    ]
+  },
+  {
+    title: 'test',
+    label: '테스트',
+    platform: 'roll20',
+    npcs: [
+    ],
+    logs: [
+      {
+        index: '1',
+        name: '1화 테스트',
+        images: [
+          {
+            id: ``,
+            file: '',
+          },
+        ],
+        interfaces: [
+          {
+            id: ``,
+            file: '',
+          },
+        ],
+      },
+    ]
+  }
+]
